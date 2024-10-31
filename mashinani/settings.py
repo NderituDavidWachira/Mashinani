44418a1eb83e51936924d54ffba825e5a055a8b6
Bsclmr270822 <133857013+Bsclmr270822@users.noreply.github.com>
2024-10-31 12:59:29 +0300

    host

--- mashinani/settings.py
+++ mashinani/settings.py
@@ -171,7 +171,7 @@ WSGI_APPLICATION = 'mashinani.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-POSTGRES_LOCALLY = 
+POSTGRES_LOCALLY = True
 DATABASES = {}
 if DEBUG == False or POSTGRES_LOCALLY == True:
     DATABASES = {
